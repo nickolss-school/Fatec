ccd1abfa987a8d1b365c6fecbdf3664842d5c902
Nickolas Maia de Araujo <99838582+nickolss@users.noreply.github.com>
2025-02-14 12:45:47 -0300

    fix: fixed bugs and issues

--- estrutura-dados/12-02-25/MinhaPrimeiraED/src/main/java/MinhaPrimeiraED.java
+++ estrutura-dados/12-02-25/MinhaPrimeiraED/src/main/java/MinhaPrimeiraED.java
@@ -1,87 +1,150 @@
 public class MinhaPrimeiraED {
     public Object[] objetos = new Object[10];
-
     public int totalDeObjetos = 0;
 
-    public void adiciona(int posicao, Object objeto) {
-        if (posicaoValida(posicao) && totalDeObjetos < objetos.length) {
-            for (int i = totalDeObjetos; i > posicao; i--) {
-                objetos[i] = objetos[i - 1];
-            }
-            objetos[posicao] = objeto;
-            totalDeObjetos++;
-        } else if (totalDeObjetos == objetos.length) {
-            Object[] novoVetor = new Object[objetos.length * 2];
-            for (int i = 0; i < objetos.length; i++) {
-                novoVetor[i] = objetos[i];
-            }
-            objetos = novoVetor;
-
-            for (int i = totalDeObjetos; i > posicao; i--) {
-                objetos[i] = objetos[i - 1];
-            }
-            objetos[posicao] = objeto;
-            totalDeObjetos++;
-        }
-    }
-
+    /*
+    A operação public void adiciona(Object objeto){..}
+    deve adicionar elementos no vetor respeitando a ordem do índice.
+    Caso não haja mais posições válidas, não adicionar.
+    */
     public void adiciona(Object objeto) {
-        if (totalDeObjetos == objetos.length) {
-            Object[] novoVetor = new Object[objetos.length * 2];
-            for (int i = 0; i < objetos.length; i++) {
-                novoVetor[i] = objetos[i];
-            }
-            objetos = novoVetor;
+        if (!cheio()) {
+            objetos[totalDeObjetos] = objeto;
+            totalDeObjetos++;
         }
-
-        objetos[totalDeObjetos] = objeto;
-        totalDeObjetos++;
     }
 
+    /*
+    A operação public void adiciona(int posicao, Object objeto){..}
+    deve adicionar um objeto numa determinada posição do vetor.
+    Caso a posição esteja ocupada, deslocar os elementos uma posição à direita para
+    não sobrepor o novo objeto.
+    */
 
+    public void adiciona(int posicao, Object objeto) {
+        if (posicaoValida(posicao)) {
+
+            if (posicaoOcupada(posicao)) {
+
+                if (cheio()) {
+                    for (int i = totalDeObjetos; i > posicao; i--) {
+                        objetos[i] = objetos[i - 1];
+                    }
+                    objetos[posicao] = objeto;
+                    totalDeObjetos++;
+                } else {
+                    for (int i = totalDeObjetos; i > posicao; i--) {
+                        objetos[i] = objetos[i - 1];
+                    }
+                    objetos[posicao] = objeto;
+                    totalDeObjetos++;
+                }
+
+            } else if (!posicaoOcupada(posicao) && !cheio()) {
+                for (int i = totalDeObjetos; i > posicao; i--) {
+                    objetos[i] = objetos[i - 1];
+                }
+                objetos[posicao] = objeto;
+                totalDeObjetos++;
+            }
+
+        } else {
+            System.out.println("Posição Inválida");
+        }
+    }
+
+    /*
+    A operação private boolean posicaoOcupada(int posicao){..}
+    deve verificar se a posição está ou não ocupada.
+    Retornar True para ocupada e False para livre.
+    */
     public boolean posicaoOcupada(int posicao) {
         return objetos[posicao] != null;
     }
 
+    /*
+    A operação private boolean posicaoValida(int posicao){..},
+    deve retornar True se a posição for válida.
+    Uma posição é válida se estiver entre os índices 0 e a última posição ocupada do vetor.
+    */
     public boolean posicaoValida(int posicao) {
         return posicao >= 0 && posicao < objetos.length;
     }
 
+    /*
+    A operação public void remove(int posicao){..}
+    deve remover o elemento de uma determinada posição do vetor.
+    No entanto, como a sequência dos índices não poderá ser pulada,
+    se houver elementos nas posições à direita do vetor, você deverá
+    deslocar todos os elementos para uma posição anterior.
+    */
     public void remove(int posicao) {
         if (posicaoValida(posicao) && posicaoOcupada(posicao)) {
             for (int i = posicao; i < totalDeObjetos - 1; i++) {
                 objetos[i] = objetos[i + 1];
             }
+
             objetos[totalDeObjetos - 1] = null;
             totalDeObjetos--;
         }
     }
 
+    /*
+    A operação public boolean contem(Object objeto){..}
+    deve retornar True se o objeto pesquisado for encontrado no vetor.
+    Implemente uma pesquisa linear conforme vimos em classe.
+     */
     public boolean contem(Object objeto) {
-        for (int i = 0; i < totalDeObjetos; i++) {
-            if (objetos[i].equals(objeto)) {
+        for (Object objetoNoVetor : objetos) {
+            if (objeto.equals(objetoNoVetor)) {
                 return true;
             }
         }
         return false;
     }
 
+    /*
+    A operação public Object getObjeto(int posicao){...}
+    deve retornar o objeto de uma determinada posição.
+     */
     public Object getObjeto(int posicao) {
         if (posicaoValida(posicao) && posicaoOcupada(posicao)) {
             return objetos[posicao];
         }
+
         return null;
     }
 
+    /*
+    A operação public int tamanho(){..}
+    deve retornar quantos elementos existem no vetor.
+     */
     public int tamanho() {
         return totalDeObjetos;
     }
 
-    public boolean cheio() {
-        return totalDeObjetos == objetos.length;
-    }
-
     public boolean vazio() {
         return totalDeObjetos == 0;
+    }
+
+    /*
+    Implemente uma operação que verifique se o vetor está cheio.
+    O vetor estará cheio quando o total de objetos é igual ao tamanho do vetor.
+    Sempre que o vetor estiver cheio, criar um novo vetor cujo tamanho deve
+    ser o dobro do anterior, copiar os elementos para o novo vetor.
+    Com esta operação, as operações adiciona sempre vão funcionar.
+     */
+    public boolean cheio() {
+        if (objetos.length == totalDeObjetos) {
+            Object[] novoVetor = new Object[objetos.length * 2];
+
+            for (int i = 0; i < objetos.length; i++) {
+                novoVetor[i] = objetos[i];
+            }
+
+            objetos = novoVetor;
+            return true;
+        }
+        return false;
     }
 }
