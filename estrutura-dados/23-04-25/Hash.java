f32cfd237c42c8ab6198c2d223014b687fdb24a2
Nickolas Maia de Araujo <99838582+nickolss@users.noreply.github.com>
2025-04-23 22:27:11 -0300

    feat: added ed class on 23-04-25

---
+++ estrutura-dados/23-04-25/Hash.java
@@ -0,0 +1,12 @@
+import java.io.*;   // pacote contem classe Scanner
+import java.util.*; // pacote contem System.in
+import java.lang.*;
+
+public class Hash {
+	  public double item;
+	  public boolean ocupado;
+	  
+	  public Hash(boolean b) { // construtor
+	    ocupado = false;
+	  }
+}
